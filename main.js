65091f00b78562fb81ad867ccbf77839ed1784b0
Luca-Bax <141884258+Bassi-Luca@users.noreply.github.com>
2024-02-13 11:21:20 -0600

    Actualizacion
    PreEntrega 1 > Cambio minusc var y func + while func login

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
-let NombreUsuario;
-let Cliente;
-let TipoUsuario;
-let tipoServicio;
+let nombreUsuario;
+let cliente;
+let tipoUsuario;
+let servicio;
 
 // Funcion de Login
-function Login() {
-  TipoUsuario = prompt(
+function login() {
+  // Richiesta di input all'inizio del ciclo
+  tipoUsuario = prompt(
     "Hola! Bienvenido a [NombreAPP]! \n 1.- Cliente \n 2.- Nuevo cliente"
   );
 
-  switch (TipoUsuario) {
+  // Il ciclo continua finché l'input non è valido
+  while (tipoUsuario === "" || (tipoUsuario !== "1" && tipoUsuario !== "2")) {
+    alert("Opción no válida. Por favor, elige 1 o 2.");
+    
+    // Richiesta di un nuovo input all'interno del ciclo
+    tipoUsuario = prompt(
+      "Hola! Bienvenido a [NombreAPP]! \n 1.- Cliente \n 2.- Nuevo cliente"
+    );
+  }
+
+  // Dopo aver ottenuto un input valido, esegui il resto del codice
+  switch (tipoUsuario) {
     case "1":
-      NombreUsuario = prompt(
+      nombreUsuario = prompt(
         "Introduce tu correo de inscripción! \n (Correo correcto: luca@coder.com)"
       );
       for (let i = 3; i > 0; i--) {
-        if (NombreUsuario === "luca@coder.com") {
-          alert("¡Hola! " + NombreUsuario + "\n ¡Bienvenido a [NombreAPP]!");
-          Menu();
-          TipoServicio();
+        if (nombreUsuario === "luca@coder.com") {
+          alert("¡Hola! " + nombreUsuario + "\n ¡Bienvenido a [NombreAPP]!");
+          menu();
+          tipoServicios();
           return;
         } else {
           alert(
@@ -26,7 +38,7 @@ function Login() {
               i +
               " intentos! \n (correo correcto: luca@coder.com)"
           );
-          NombreUsuario = prompt(
+          nombreUsuario = prompt(
             "Introduce tu correo: \n (RECUERDA: correo correcto: luca@coder.com)"
           );
         }
@@ -37,49 +49,46 @@ function Login() {
       break;
 
     case "2":
-      Cliente = prompt("Introduce tu nombre:");
-      while (Cliente === "") {
-        Cliente = prompt(
+      cliente = prompt("Introduce tu nombre:");
+      while (cliente === "") {
+        cliente = prompt(
           "Formato de dato incompleto y/o incorrecto! intenta otra vez! \n\n (Si valor es vacio sigue infinito) \n Introduce tu correo:"
         );
       }
       alert(
         "¡Gracias " +
-          Cliente +
+          cliente +
           "! ¡Ahora puedes utilizar todos nuestros servicios!"
       );
-      Menu();
-      TipoServicio();
-      break;
-
-    default:
-      alert("Opción no válida. Por favor, elige 1 o 2.");
+      menu();
+      tipoServicios();
       break;
   }
 }
 
 // Funcion de Menu
-function Menu() {
-  if (TipoUsuario === "1") {
-    alert(NombreUsuario + "! \n Elige los servicios!");
-  } else if (TipoUsuario === "2") {
+function menu() {
+  if (tipoUsuario === "1") {
+    alert(nombreUsuario + "! \n Elige los servicios!");
+  } else if (tipoUsuario === "2") {
     alert(
-      Cliente +
+      cliente +
         "! \n Somos felices que estés con nosotros en [NombreAPP]! \n En que te podemos auydar?"
     );
   }
 }
 
 // Funcion Servicios
-function TipoServicio() {
-  tipoServicio = prompt(" 1.- Limpieza \n 2.- Hospedaje \n 3.- Paseo");
-  while (tipoServicio === "" || (tipoServicio !== "1" && tipoServicio !== "2" && tipoServicio !== "3")){
+function tipoServicios() {
+  servicio = prompt(" 1.- Limpieza \n 2.- Hospedaje \n 3.- Paseo");
+  while (
+    servicio === "" ||
+    (servicio !== "1" && servicio !== "2" && servicio !== "3")
+  ) {
     alert("Elejir unos de nuestros servicios! \n \n");
-    TipoServicio();
+    servicio = prompt(" 1.- Limpieza \n 2.- Hospedaje \n 3.- Paseo");
   }
-  return;
-  
 }
 
-Login();
+login();
 alert("FIN PRE ENTREGA 1");
